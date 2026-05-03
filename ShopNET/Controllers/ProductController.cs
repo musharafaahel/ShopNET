cc629efeb4ceea3d6c5507465a95fec8cc0b6aec
musharafaahel <musaahel@gmail.com>
2026-05-03 13:19:18 +0530

    Step 3 - Added routing and product details page

--- ShopNET/Controllers/ProductController.cs
+++ ShopNET/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using ShopNET.Models;
 
 namespace ShopNET.Controllers
@@ -16,5 +17,28 @@ namespace ShopNET.Controllers
             };
             return View(products);
         }
+
+        public IActionResult Details(int id)
+        {
+            //Temporary fake data for demonstration purposes
+            var products = new List<Product>
+            {
+                new Product { Id = 1, Name = "Wireless Headphones", Description = "Premium sound quality", Price = 99.99m, Stock = 50 },
+                new Product { Id = 2, Name = "Mechanical Keyboard", Description = "Tacticle and fast", Price = 149.99m, Stock = 30 },
+                new Product { Id = 3, Name = "USB-C Hub", Description = "7 ports in one", Price = 49.99m, Stock = 100 },
+
+            };
+            //Find the product using matching id
+            var product = products.FirstOrDefault(p => p.Id == id);
+
+            //If product is not found, return NotFound result 404 page
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
     }
+
+    
 }
